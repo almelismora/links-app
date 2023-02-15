ad678eae1dfc823d5e3152ae83c0026c3e0543ec
Elis Mora <almedinaelis.mora@gmail.com>
2023-02-15 09:28:36 +0100

    formulario y navbar hechos

--- routes/links.js
+++ routes/links.js
@@ -3,12 +3,21 @@ const router = express.Router();
 const pool = require('../database')
 
 /* GET users listing. */
-router.get('/',async  (req, res, next) => {
+router.get('/', async  (req, res, next) => {
   const [ result ] = await pool.query('SELECT 1 + 1')
   res.json(result)
   //res.send('LINKS!!!');
 });
 
+
+router.get('/add', (req, res) => {
+  res.render('links/add')
+})
+
+
+router.post('/add', (req, res) => {
+  res.send('Received')
+})
 
 
 module.exports = router;
