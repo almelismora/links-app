271f556b16d9a841880ac57f8a14ae3f0e101d07
Elis Mora <almedinaelis.mora@gmail.com>
2023-02-15 13:42:08 +0100

    Añadidos mensajes con flash

--- app.js
+++ app.js
@@ -4,44 +4,59 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const { engine } = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
+const smysql = require('express-mysql-session')
+const { database } = require('./keys.js')
 
 const indexRouter = require('./routes/index');
 const linksRouter = require('./routes/links');
-const authRouter = require('./routes/authentication');
+const authenticationRouter = require('./routes/authentication');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', engine({
-  defaultLayout: 'main', 
-  layoutsDir: path.join(app.get('views'), 'layouts'),
-  partialsDir: path.join(app.get('views'), 'partials'),
+  defaultLayout: 'main',
+  layoutsDir:  path.join(app.get('views'), 'layouts'),
+  partialsDir:  path.join(app.get('views'), 'partials'),
   extname: '.hbs',
   helpers: require('./lib/handlebars')
 }))
 
 app.set('view engine', '.hbs');
 
-// middlewares
+//Middlewares
+app.use( session ({
+  secret: 'patata',
+  resave: false,
+  saveUninitialized: false,
+  store: new smysql(database)
+  })
+)
+
+app.use(flash())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// variables globales
+
+//Global Variables
 app.use((req, res, next) => {
 
+  app.locals.success = req.flash('success')
   next()
 
 })
-
-// routes 
+//Routes
 app.use('/', indexRouter);
 app.use('/links', linksRouter);
-app.use('/authentication', authRouter);
+app.use('/authentication', authenticationRouter);
 
-// public
+
+//Public
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
--- routes/links.js
+++ routes/links.js
@@ -1,23 +1,60 @@
 const express = require('express');
 const router = express.Router();
+
 const pool = require('../database')
 
 /* GET users listing. */
-router.get('/', async  (req, res, next) => {
-  const [ result ] = await pool.query('SELECT 1 + 1')
-  res.json(result)
-  //res.send('LINKS!!!');
+router.get('/', async (req, res, next) => {
+  const [ links ] = await pool.query('SELECT * FROM links')
+  console.log(links)
+  res.render('links/list', { links })
+  
 });
-
 
 router.get('/add', (req, res) => {
   res.render('links/add')
 })
 
+router.post('/add', async (req, res) => {
 
-router.post('/add', (req, res) => {
-  res.send('Received')
+  const { title, url, description } = req.body
+  const newLink = {
+    title,
+    url,
+    description
+  }
+
+  await pool.query('INSERT INTO links SET ?', [newLink])
+  req.flash('success', 'link has been saved successfully')
+  res.redirect('/links')
 })
 
+
+router.get('/delete/:id', async (req, res) => {
+  const { id } = req.params
+  await pool.query('DELETE FROM links WHERE id = ?', [id])
+  req.flash('success', 'link has been removed successfully')
+  res.redirect('/links')
+}
+)
+router.get('/edit/:id', async (req, res) => {
+  const { id } = req.params
+  const [ link ] = await pool.query('SELECT * FROM links WHERE id = ?', [id])
+  console.log(link)
+  res.render('links/edit', {link:link[0]})
+})
+
+router.post('/edit/:id', async (req, res) => {
+  const { id } = req.params
+  const { title, url, description} = req.body
+  const newLink = {
+    title,
+    url,
+    description
+  }
+  await pool.query('UPDATE links SET ? WHERE id = ?', [newLink, id])
+  req.flash('success', 'link has been saved successfully')
+  res.redirect('/links')
+})
 
 module.exports = router;
